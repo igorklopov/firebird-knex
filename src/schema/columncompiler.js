5c1ee9a633c4b4a48759d23f8d58f8f0af58b96f
igorklopov <igor@klopov.com>
2016-07-18 11:46:52 +0300

    "increments", but still not works properly

--- src/schema/columncompiler.js
+++ src/schema/columncompiler.js
@@ -1,4 +1,5 @@
 import { Client } from 'knex';
+import { assign } from 'lodash';
 import inherits from 'inherits';
 
 const ColumnCompiler = Client.prototype.ColumnCompiler;
@@ -8,5 +9,11 @@ function ColumnCompiler_Firebird() {
   this.modifiers = [ 'nullable' ];
 }
 inherits(ColumnCompiler_Firebird, ColumnCompiler);
+
+assign(ColumnCompiler_Firebird.prototype, {
+
+  increments: 'int not null primary key'
+
+});
 
 export default ColumnCompiler_Firebird;
