e20f52df0f6b55c175fc02f7752e5b321eee2ccc
igorklopov <igor@klopov.com>
2016-07-20 15:25:05 +0300

    let it look like query/status-based knex transaction handling

--- src/transaction.js
+++ src/transaction.js
@@ -2,6 +2,7 @@ import { Client, Promise } from 'knex';
 import { assign } from 'lodash';
 import inherits from 'inherits';
 
+const debug = require('debug')('knex:tx');
 const Transaction = Client.prototype.Transaction;
 
 function Transaction_Firebird() {
@@ -26,35 +27,44 @@ assign(Transaction_Firebird.prototype, {
   },
 
   commit(conn, value) {
-    return new Promise((resolve, reject) => {
-      conn._transaction.commit((error) => {
-        if (error) return reject(error);
-        delete conn._transaction;
-        this._resolver(value);
-        this._completed = true;
-        resolve();
-      });
-    });
+    return this.query(conn, 'commit', 1, value);
   },
 
   release() {
     throw new Error('releasing savepoints not implemented');
   },
 
-  rollback(conn, value) {
-    return new Promise((resolve, reject) => {
-      conn._transaction.rollback((error) => {
-        if (error) return reject(error);
-        delete conn._transaction;
-        this._rejecter(value);
-        this._completed = true;
-        resolve();
-      });
-    });
+  rollback(conn, error) {
+    return this.query(conn, 'rollback', 2, error);
   },
 
   rollbackTo() {
     throw new Error('rolling back to savepoints not implemented');
+  },
+
+  query(conn, method, status, value) {
+    const q = new Promise((resolve, reject) => {
+        const transaction = conn._transaction;
+        delete conn._transaction;
+        transaction[method]((error) => {
+          if (error) return reject(error);
+          resolve();
+        });
+      })
+      .catch((error) => {
+        status = 2;
+        value = error;
+        this._completed = true;
+        debug('%s error running transaction query', this.txid);
+      })
+      .tap(() => {
+        if (status === 1) this._resolver(value);
+        if (status === 2) this._rejecter(value);
+      });
+    if (status === 1 || status === 2) {
+      this._completed = true;
+    }
+    return q;
   }
 
 });
